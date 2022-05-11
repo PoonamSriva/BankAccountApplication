ac0e552b1b12088e85a77091f13d765069a3b10c
PoonamSriva <psinha08@outlook.com>
2022-05-11 22:49:12 +0200

    added template

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -11,4 +11,11 @@ class BankAccountTest {
         assertEquals(500, bankAccount.getBalance());
     }
 
+
+    @Test
+    public void shouldDeposit500WhenWeHave1000Balance(){
+        bankAccount.deposit(500);
+        assertEquals(1500, bankAccount.getBalance());
+    }
+
 }
