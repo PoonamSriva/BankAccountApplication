9c1cc97f8faacb2a1d3b8b9538212f12084c890e
PoonamSriva <psinha08@outlook.com>
2022-05-15 22:13:43 +0200

    introduced minimumbalnce concept

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,12 +1,15 @@
 public class BankAccount {
     private double balance;
+    private double minimumBalance;
 
     public BankAccount() {
         balance = 0;
+        minimumBalance = 0;
     }
 
-    public BankAccount(double initialBalance) {
+    public BankAccount(double initialBalance, double minimumBalance) {
         balance = initialBalance;
+        this.minimumBalance = minimumBalance;
     }
 
     public void deposit(double amount) {
@@ -18,7 +21,7 @@ public class BankAccount {
 
     public void withdraw(double amount) {
         if (amount >= 0) {
-            if (balance - amount >= 0) {
+            if (balance - amount >= minimumBalance) {
                 balance = balance - amount;
             }
         }
--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -7,8 +7,25 @@ public class BankAccountTest {
 
 
     @Test
+    public void shouldKeepMinimumBalanceOf100() {
+        bankAccount = new BankAccount(1000, 100);
+        assertEquals(1000, bankAccount.getBalance());
+        bankAccount.withdraw(1000);
+        assertEquals(1000, bankAccount.getBalance());
+    }
+
+    @Test
+    public void shouldKeepMinimumBalanceOf200() {
+        bankAccount = new BankAccount(1000, 200);
+        assertEquals(1000, bankAccount.getBalance());
+        bankAccount.withdraw(900);
+        assertEquals(1000, bankAccount.getBalance());
+    }
+
+
+    @Test
     public void shouldNotWithdraw500WhenWeHave0Balance() {
-        bankAccount = new BankAccount(0);
+        bankAccount = new BankAccount(0, 0);
         assertEquals(0, bankAccount.getBalance());
         bankAccount.withdraw(500);
         assertEquals(0, bankAccount.getBalance());
@@ -16,7 +33,7 @@ public class BankAccountTest {
 
     @Test
     public void shouldWithdraw500WhenWeHave1000Balance() {
-        bankAccount = new BankAccount(1000);
+        bankAccount = new BankAccount(1000, 0);
         assertEquals(1000, bankAccount.getBalance());
         bankAccount.withdraw(500);
         assertEquals(500, bankAccount.getBalance());
@@ -24,7 +41,7 @@ public class BankAccountTest {
 
     @Test
     public void shouldNotWithdrawNegative500WhenWeHave1000Balance() {
-        bankAccount = new BankAccount(1000);
+        bankAccount = new BankAccount(1000, 0);
         assertEquals(1000, bankAccount.getBalance());
 
         bankAccount.withdraw(-500);
@@ -34,7 +51,7 @@ public class BankAccountTest {
 
     @Test
     public void shouldNotDepositNegative500WhenWeHave1000Balance() {
-        bankAccount = new BankAccount(1000);
+        bankAccount = new BankAccount(1000, 0);
         assertEquals(1000, bankAccount.getBalance());
         bankAccount.deposit(-500); // nothing should happend since negative is not allowed
         assertEquals(1000, bankAccount.getBalance());
@@ -43,7 +60,7 @@ public class BankAccountTest {
 
     @Test
     public void shouldDeposit500WhenWeHave1000Balance() {
-        bankAccount = new BankAccount(1000);
+        bankAccount = new BankAccount(1000, 0);
         assertEquals(1000, bankAccount.getBalance());
         bankAccount.deposit(500);
         assertEquals(1500, bankAccount.getBalance());
